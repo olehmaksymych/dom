a2ad437e449ecfa76d3daffc5be24427c846b04f
olehmaksymych <90643849+olehmaksymych@users.noreply.github.com>
2022-09-20 17:46:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,106 +1,169 @@
-let menu = document.querySelector("#menu-bars");
-let navbar = document.querySelector(".navbar");
+let content = document.querySelector('.content');
 
-menu.onclick = () => {
-  menu.classList.toggle("fa-times");
-  navbar.classList.toggle("active");
-};
+let aside = document.createElement('nav');
+aside.classList.add('asideBlock');
+content.append(aside);
 
-window.onscroll = () => {
-  menu.classList.remove("fa-times");
-  navbar.classList.remove("active");
-};
+let main = document.createElement('main');
+main.classList.add('mainBlock');
+content.append(main);
 
-document.querySelector("#search-icon").onclick = () => {
-  document.querySelector("#search-form").classList.toggle("active");
-};
+let arrOfClubs = [
+    {
+        title: 'The most titled footbal clubs',
+        dataName: '1',
+        description: [
+            `This is a brief description of the main football clubs in Western
+            European Countries. Please, click on the country you want to find out
+            what football club is the most titled club in the whole history of
+            football in the selected country.`,`The names of the football leagues are:`,     
+            `England - "Premier League"`, `Germany - "The Bundesliga"`,
+            `Italy - "Serie A"`,`Spain - "La Liga"`,`France - "Ligue 1"`
+        ],
+        img: 'images/futballClubs.jfif',
+        alt: 'Titled clubs',
+        source: `https://goalballlive.com/football-club-with-most-trophies-in-the-world/`,
+        class: 'clubs'
+    },
+    {
+        title: 'Liverpool',
+        dataName: '2',
+        description: [
+            `Liverpool is one of the most successful English clubs in terms
+          of trophies of all time. It is the the most
+          successful club in England.
+          The 'Merseyside's' have won 19 Premier League Cups and six Uefa Champions League
+          Cups. In total, Liverpool has 43 titles.`
+        ],
+        img: 'images/footbal_clubs/liverpool.png',
+        alt: 'Liverpool',
+        source: `https://www.liverpoolfc.com/`,
+        class: 'liverpool'
+    },
+    {
+        title: 'Bayern Munich',
+        dataName: '3',
+        description: [
+            `FC Bayern München is a German football club from Munich.`, `One of the
+            most famous and strongest clubs in Germany and the world.`,
+            `Bayern Munich has 84 trophies, 32 of which are Bundesliga cups and 6
+            UEFA Champions League cups.`
+        ],
+        img: 'images/footbal_clubs/bayern_munich.png',
+        alt: 'Bavaria',
+        source: `https://fcbayern.com/en`,
+        class: 'bavaria'
+    },
+    {
+        title: 'Juventus',
+        dataName: '4',
+        description: [
+            `Juventus Football Club is a football club from the city of Turin, one
+          of the oldest clubs in Italy, and one of the most titled clubs in
+          Europe and the world.`,`This club has a record 36 Serie A cups, as
+          well as 2 UEFA Champions League cups.`,
+          `In total, Juve has total of 75 prestigious trophies.`
+        ],
+        img: 'images/footbal_clubs/juventus.png',
+        alt: 'juve',
+        source: `https://www.juventus.com/en/`,
+        class: 'juve'
+    },
+    {
+        title: 'Real',
+        dataName: '5',
+        description: [
+            `This club is the best club in Spain in the history of Spanish
+          football.`,`The assets of this club include 99 trophies.`, `The club
+          became the champion of "La Liga" 35 times and won the Champions League
+          the maximum number of times - 14.`,
+          `Real Madrid is recognized as the best football club of the 20th
+          century according to FIFA.`
+        ],
+        img: 'images/footbal_clubs/real.png',
+        alt: 'Yeezus',
+        source: `https://www.realmadrid.com/en`,
+        class: 'real'
+    },
+    {
+        title: 'Olympique Lyonnais',
+        dataName: '6',
+        description: [
+            `Founded in 1950, the club won its first Ligue 1 championship in 2002, starting a national record-setting streak of seven successive titles.`, 
+            `Lyon has also won eight Trophées des Champions, five Coupes de France, and three Ligue 2 titles.`,
+            `Ol has a total of 35 trophies.`
+        ],
+        img: 'images/footbal_clubs/ol.png',
+        alt: 'ol',
+        source: `https://www.ol.fr/fr`,
+        class: 'ol'
+    }
+];
 
-document.querySelector("#close").onclick = () => {
-  document.querySelector("#search-form").classList.remove("active");
-};
+arrOfClubs.forEach(item => {
+    renderAsideItems(item);
+});
 
-let sectionItem = document.querySelector(".section_item");
-let navbar_item = document.querySelector(".navbar_item");
-let home = document.querySelector("#home");
-let england = document.querySelector("#england");
-let germany = document.querySelector("#germany");
-let italy = document.querySelector("#italy");
-let spain = document.querySelector("#spain");
-let france = document.querySelector("#france");
+function renderAsideItems(item) {
+    let button = document.createElement('button');
+    button.classList.add('asideBtn');
+    button.dataset.attribute = `${item.dataName}`;
+    button.textContent = `${item.title}`;
+    aside.append(button);
+}
 
-let home_page = document.querySelector("#home_page");
-let manchester = document.querySelector("#manchester");
-let bavaria = document.querySelector("#bavaria");
-let juve = document.querySelector("#juve");
-let real = document.querySelector("#real");
-let psg = document.querySelector("#psg");
+let asideBtn = document.querySelectorAll('.asideBtn');
+asideBtn[0].classList.add('clicked');
+addToMain(arrOfClubs[0]);
 
-document.querySelector("#open_logo").onclick = () => {
-  home_page.style.display = "block";
-  navbar.classList.toggle("active");
-  manchester.style.display = "none";
-  juve.style.display = "none";
-  real.style.display = "none";
-  psg.style.display = "none";
-  bavaria.style.display = "none";
-};
+document.querySelector('.asideBlock').addEventListener('click', function(e) {
+    if (e.target && e.target.classList.contains('asideBtn')) {
+        renderMain(e);
+    }
+});
 
-home.onclick = () => {
-  home_page.style.display = "block";
-  navbar.classList.toggle("active");
-  manchester.style.display = "none";
-  juve.style.display = "none";
-  real.style.display = "none";
-  psg.style.display = "none";
-  bavaria.style.display = "none";
-};
 
-england.onclick = () => {
-  manchester.style.display = "block";
-  navbar.classList.toggle("active");
-  home_page.style.display = "none";
-  juve.style.display = "none";
-  real.style.display = "none";
-  psg.style.display = "none";
-  bavaria.style.display = "none";
-};
+function renderMain(event) {
+    document.querySelector('.asideBtn.clicked').classList.remove('clicked');
+    event.target.classList.add('clicked');
+    let thatTarget = event.target.dataset.attribute;
+    main.innerHTML = '';
 
-spain.onclick = () => {
-  real.style.display = "block";
-  navbar.classList.toggle("active");
-  home_page.style.display = "none";
-  juve.style.display = "none";
-  manchester.style.display = "none";
-  psg.style.display = "none";
-  bavaria.style.display = "none";
-};
+    let mainItem = arrOfClubs.find(function({dataName}) {
+        return dataName === thatTarget;
+    });
 
-germany.onclick = () => {
-  bavaria.style.display = "block";
-  navbar.classList.toggle("active");
-  home_page.style.display = "none";
-  juve.style.display = "none";
-  real.style.display = "none";
-  psg.style.display = "none";
-  manchester.style.display = "none";
-};
+    addToMain(mainItem);
+}
 
-italy.onclick = () => {
-  juve.style.display = "block";
-  navbar.classList.toggle("active");
-  home_page.style.display = "none";
-  manchester.style.display = "none";
-  real.style.display = "none";
-  psg.style.display = "none";
-  bavaria.style.display = "none";
-};
+function addToMain(item) {
+    content.classList = "";
+    content.classList.add('content', item.class);
 
-france.onclick = () => {
-  psg.style.display = "block";
-  navbar.classList.toggle("active");
-  home_page.style.display = "none";
-  juve.style.display = "none";
-  real.style.display = "none";
-  manchester.style.display = "none";
-  bavaria.style.display = "none";
-};
+    main.innerHTML = `
+        <div class='mainHeader'>${item.title}</div>
+        <div class='mainContent'>
+            <img class='mainImg' src='${item.img}' alt='${item.alt}' />
+            <div class='mainDescr'></div>
+            </div>
+        <div class='mainSource'>
+            <iframe class="iFrame" style="border-radius:12px" src=${item.source} width="100%" height="320" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        </div>
+    `;
+
+    document.querySelector('.mainDescr').append(createUl(item));
+}
+
+function createUl(item) {
+    let ul = document.createElement('ul');
+    ul.classList.add('mainDescrUl');
+
+    item.description.forEach(elem => {
+        let li = document.createElement('li');
+        li.classList.add('mainDescrLi');
+        li.textContent = elem;
+        ul.append(li);
+    });
+
+    return ul;
+}
